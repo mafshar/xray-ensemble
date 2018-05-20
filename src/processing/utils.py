a2ca4d7c45deea1897d165cfcd61244192389bde
mafshar <ma2510@nyu.edu>
2018-05-20 14:38:24 -0400

    utility helper functions

--- src/processing/utils.py
+++ src/processing/utils.py
@@ -2,7 +2,32 @@ import os
 import glob
 import sys
 
+from shutil import move, copyfile
+
 def mkdir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
     return
+
+def mv(src, dst):
+    try:
+        move(src, dst)
+    except:
+        print 'File', src, 'does not exist'
+    return
+
+def cp(src, dst):
+    try:
+        copyfile(src, dst)
+    except:
+        print 'File', src, 'does not exist'
+    return
+
+def read_textfile_by_line(file):
+    content = None
+    with open(file) as fh:
+        content = fh.readlines()
+
+    if content:
+        content = map(lambda x: x.strip(), content)
+    return content
