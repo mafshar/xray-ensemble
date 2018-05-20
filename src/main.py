ef4f6851315d2333c09be7e0204ee170f1d077e3
mafshar <ma2510@nyu.edu>
2018-05-20 15:53:47 -0400

    added dataset ingestion for training and testing

--- src/main.py
+++ src/main.py
@@ -10,9 +10,11 @@ warnings.filterwarnings('ignore')
 
 import numpy as np
 
-from processing import datasets
+from processing import datasets, transformations
+from torchvision import transforms, utils
 
 DATA_DIR = './data'
+IMG_SIZE = 256
 
 input_partitioning_flag = False
 
@@ -23,8 +25,24 @@ if __name__ == '__main__':
         datasets.join_filename_and_labels(data_path=DATA_DIR)
         datasets.partition_images(data_path=DATA_DIR)
 
-    train_dataset = datasets.XrayDataset('hernia', 'No Finding', mode='train')
-    print train_dataset[0]
+    scale = transformations.Rescale(IMG_SIZE)
+    tensorize = transformations.ToTensor()
+    composed_transforms = transforms.Compose(
+        [scale,
+        tensorize,
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
-    test_dataset = datasets.XrayDataset('hernia', 'No Finding', mode='test')
-    print len(test_dataset)
+    train_dataset = datasets.XrayDataset(
+        'hernia',
+        'No Finding',
+        mode='train',
+        transform=composed_transforms)
+
+    test_dataset = datasets.XrayDataset(
+        'hernia',
+        'No Finding',
+        mode='test',
+        transform=composed_transforms)
+
+    
