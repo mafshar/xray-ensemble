032143381ff05e405f01240eb39bd7e323fa0b27
mafshar <ma2510@nyu.edu>
2018-05-20 14:38:50 -0400

    setup pipeline for data partitioning and data ingestion

--- src/main.py
+++ src/main.py
@@ -10,6 +10,21 @@ warnings.filterwarnings('ignore')
 
 import numpy as np
 
+from processing import datasets
+
 DATA_DIR = './data'
 
+input_partitioning_flag = False
+
+
 if __name__ == '__main__':
+    if input_partitioning_flag:
+        datasets.initialize_data()
+        datasets.join_filename_and_labels(data_path=DATA_DIR)
+        datasets.partition_images(data_path=DATA_DIR)
+
+    train_dataset = datasets.XrayDataset('hernia', 'No Finding', mode='train')
+    print train_dataset[0]
+
+    test_dataset = datasets.XrayDataset('hernia', 'No Finding', mode='test')
+    print len(test_dataset)
