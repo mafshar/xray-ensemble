62596e58627a6dcb0afa2201b7d8293a508e4889
mafshar <ma2510@nyu.edu>
2018-05-20 01:02:23 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob
+import time
+import torch
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+
+DATA_DIR = './data'
+
+if __name__ == '__main__':
