feb04f6afc012984d7d145bdb63f80dbeec97eb7
mafshar <ma2510@nyu.edu>
2018-09-09 13:05:32 -0400

    added validation to training loop

--- src/main.py
+++ src/main.py
@@ -13,11 +13,14 @@ import torch.nn as nn
 import torch.optim as optim
 
 from os.path import join
+from torchvision import transforms, utils
+from tqdm import tqdm
+
 from processing import datasets, transformations
 from processing.datasets import DATA_DIR
 from deep_models.models import MODEL_DIR
 from deep_models import models
-from torchvision import transforms, utils
+
 
 IMG_SIZE = 256
 
@@ -28,9 +31,12 @@ def train(train_loader, val_loader, model_path, num_epochs=10):
     weights = torch.randn(2)
     criterion = nn.CrossEntropyLoss(weight=weights)
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, nesterov=True)
-
-    for epoch in range(num_epochs):
-        running_loss = 0.0
+    # begin train
+    for epoch in tqdm(range(num_epochs)):
+        running_loss = 0.
+        train_loss = 0.
+        val_loss = 0.
+        # begin training
         for i, data in enumerate(train_loader, 0):
 
             inputs, labels = data
@@ -44,21 +50,43 @@ def train(train_loader, val_loader, model_path, num_epochs=10):
             loss.backward()
             optimizer.step()
 
-            # print statistics:
-            ## print after every 4 batches
-            running_loss += loss.item()
-            if i % 4 == 3:
-                print '==> epoch:\n\t%d' % (epoch + 1)
-                print '==> samples processed:\n\t%5d' % (i + 1)
-                print '==> loss:\n\t%.3f' % (running_loss / 4)
-                print '-' * 30
-                running_loss = 0.0
+            train_loss = loss.item()
+            ## @TODO: errors are done here as well
 
+        # end training
+        # begin validation
+        for i, data in enumerate(val_loader, 0):
+            inputs, labels = data
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item()
+            ## @TODO: errors are done here as well
+
+        # end validation
+        # stats
+        output_string = 'train:\n\tepoch --> | lr --> | loss --> | error --> \n'
+        output_string += 'validation:\n\tepoch --> | lr --> | loss --> | error --> \n'
+        print output_string
+    # end train
+
+    # save model
+    torch.save(net, model_path)
     return
 
-
 def evaluate(test_loader, model_path):
-    pass
+    correct = 0
+    total = 0
+    net = torch.load(model_path)
+    for i, data in enumerate(test_loader, 0):
+        inputs, labels = data
+        outputs = net(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += lables.size(0)
+        correct += (predicted == labels).sum().item()
+    print 'Accuracy: {:04.3f}%'.format(100 * (correct / total))
+    return
 
 
 if __name__ == '__main__':
@@ -73,33 +101,45 @@ if __name__ == '__main__':
         [scale,
         tensorize,
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+        ]
+    )
 
     train_dataset = datasets.XrayDataset(
         'hernia',
         'No Finding',
         mode='train',
-        transform=composed_transforms)
+        transform=composed_transforms
+    )
 
     test_dataset = datasets.XrayDataset(
         'hernia',
         'No Finding',
         mode='test',
-        transform=composed_transforms)
+        transform=composed_transforms
+    )
 
     train_loader, val_loader = datasets.train_val_split_loader(
         train_dataset,
         batch_size=4,
-        num_workers=8)
+        num_workers=8
+    )
 
     test_loader = datasets.test_loader(
         test_dataset,
         batch_size=4,
-        num_workers=8)
+        num_workers=8
+    )
 
     models.initialize_models()
+    model_path = join(MODEL_DIR, 'hernia_vs_no_findings/model_ckpt.pt')
 
     train(
         train_loader,
         val_loader,
-        join(MODEL_DIR, 'hernia_vs_no_findings'))
+        model_path
+    )
+
+    # evaluate(
+    #     test_loader,
+    #     model_path
+    # )
